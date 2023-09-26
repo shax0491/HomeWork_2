4e2de3c3d4a440f0264c5642e2e4c4c3fb517b70
AndreyT <shax0491@gmail.com>
2023-09-26 19:33:53 +0300

    Задача 1

---
+++ HomeWork_1/Program.cs
@@ -0,0 +1,6 @@
+﻿﻿Console.Write("Введите трехзначное число: "); // Ввод значения пользователем 
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondNumber = (number % 100) / 10;
+
+Console.WriteLine(secondNumber);
