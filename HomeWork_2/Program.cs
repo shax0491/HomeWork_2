00adfe7bae71bcb52086b201050cc8c41f8a2a85
AndreyT <shax0491@gmail.com>
2023-09-26 19:37:10 +0300

    Задача 2

---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,12 @@
+﻿﻿Console.Write("Введите число: "); // Ввод значения пользователем
+string number = Console.ReadLine();
+
+char[] numberArray = number.ToCharArray();
+if (numberArray.Length >= 3)
+{
+    Console.Write($"{number} -> {numberArray[2]}");
+}
+else
+{
+    Console.Write($"{number} -> третьей цифры нет");
+}
